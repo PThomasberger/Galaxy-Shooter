5d95ad611c1b9b51fbb1704c36757c5e97b955de
Pete <petethomasberger@gmail.com>
2021-04-12 18:20:03 -0400

    Created editable variable _speed in Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
