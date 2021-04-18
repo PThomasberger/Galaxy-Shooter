990d8ed8b73cfdabf99c72c8679583158e03dae1
Pete <petethomasberger@gmail.com>
2021-04-17 22:08:33 -0400

    Created Player movement boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,11 +16,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 inputDirection = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(inputDirection * _speed * Time.deltaTime);
+
+        
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 1.5f), 0);
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
