843b1b802537606e86c35a1ab287ec17eec54a9a
Pete <petethomasberger@gmail.com>
2021-04-24 09:21:17 -0400

    Created a cool down for firing the laser

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     private float _speed = 3.5f;
     [SerializeField]
     private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = .15f;
+    private float _canFire = -1f;
 
     // Start is called before the first frame update
     void Start()
@@ -47,8 +50,9 @@ public class Player : MonoBehaviour
 
     void InstantiateLaser()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
+            _canFire = Time.time + _fireRate;
             Instantiate(_laserPrefab, transform.position + new Vector3(0, .08f, 0), Quaternion.identity);
         }
     }
